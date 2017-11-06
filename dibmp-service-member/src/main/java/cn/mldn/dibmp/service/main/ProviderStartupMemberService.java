01de91be49c7e3012ef844087e92fdd9fc45c859
syuutou <1152750238@qq.com>
2017-11-06 15:32:12 +0800

    3333333

--- dibmp-service-member/src/main/java/cn/mldn/dibmp/service/main/ProviderStartupMemberService.java
+++ dibmp-service-member/src/main/java/cn/mldn/dibmp/service/main/ProviderStartupMemberService.java
@@ -4,6 +4,6 @@ import com.alibaba.dubbo.container.Main;
 
 public class ProviderStartupMemberService {
 	public static void main(String[] args) {   
-		Main.main(args); // 启动Dubbo服务       
-	}  
+		Main.main(args); // 启动Dubbo服务          
+	}   
 }
